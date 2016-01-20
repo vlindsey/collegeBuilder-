963f911dd3e4ce2738a47443e13ae0041daba03c
VLindsey <student@valerie-lindsey.local>
2016-01-20 10:10:50 -0600

    Initial Commit

---
+++ collegeProfileBuilderTests/collegeProfileBuilderTests.swift
@@ -0,0 +1,36 @@
+//
+//  collegeProfileBuilderTests.swift
+//  collegeProfileBuilderTests
+//
+//  Created by VLindsey on 1/20/16.
+//  Copyright © 2016 Valerie. All rights reserved.
+//
+
+import XCTest
+@testable import collegeProfileBuilder
+
+class collegeProfileBuilderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
