91799dab20daea8b9bf4dc6b466c6cd41c7f7558
VLindsey <student@Valerie-Lindsey.local>
2016-01-29 10:42:49 -0600

    first commit

---
+++ collegeProfileBuilder/College.swift
@@ -0,0 +1,13 @@
+//
+//  College.swift
+//  collegeProfileBuilder
+//
+//  Created by VLindsey on 1/25/16.
+//  Copyright © 2016 Valerie. All rights reserved.
+//
+
+import UIKit
+
+class College: NSObject {
+
+}
